68a4bf7fb472bd958a9aa80df2f8424ea9412f1c
bBraian <braianvoficial@gmail.com>
2023-04-20 18:49:01 -0300

    :lipstick: feat: creating home page

--- src/pages/home/components/ClaimUsernameForm/index.tsx
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -1,14 +1,30 @@
 import { Button, TextInput } from "@ignite-ui/react";
 import { Form } from "./styles";
 import { ArrowRight } from "phosphor-react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const claimUsernameFormSchema = z.object({
+    username: z.string()
+})
+
+type ClaimUsernameFormData = z.infer<typeof claimUsernameFormSchema>
 
 export function ClaimUsernameForm() {
+type ClaimUsernameFormData = z.infer<typeof claimUsernameFormSchema>
+    const { register, handleSubmit } = useForm<ClaimUsernameFormData>();
+
+    async function handleClaimUsername(data: ClaimUsernameFormData) {
+        console.log(data);
+    }
+
     return (
-        <Form as="form">
+        <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
             <TextInput 
                 size="sm" 
                 prefix="ignite.com/" 
-                placeholder="seu-usuario" 
+                placeholder="seu-usuario"
+                {...register('username')}
             />
             <Button size="sm" type="submit">
                 Reservar usuário

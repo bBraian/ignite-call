0c1cd0b55909726e138726edae29a8a75eade95e
bBraian <braianvoficial@gmail.com>
2023-05-25 23:52:19 -0300

    updating

--- src/lib/auth/prisma-adapter.ts
+++ src/lib/auth/prisma-adapter.ts
@@ -1,9 +1,41 @@
 import { Adapter } from "next-auth/adapters"
 import { prisma } from "../prisma"
+import { NextApiRequest, NextApiResponse } from "next"
+import { parseCookies, destroyCookie } from "nookies"
 
-export function PrismaAdapter(): Adapter {
+export function PrismaAdapter(req: NextApiRequest, res: NextApiResponse): Adapter {
     return {
-      async createUser(user) {},
+      async createUser(user) {
+        const { '@ignitecall:userId': userIdOnCookies } = parseCookies({ req })
+
+        if(!userIdOnCookies) {
+            throw new Error('Usuário não encontrado nos Coolies.')
+        }
+
+        const prismaUser = await prisma.user.update({
+            where: {
+                id: userIdOnCookies
+            },
+            data: {
+                name: user.name,
+                email: user.email,
+                avatar_url: user.avatar_url
+            }
+        })
+
+        destroyCookie({ res }, '@ignitecall:userId', {
+            path: '/'
+        })
+
+        return {
+            id: prismaUser.id,
+            name: prismaUser.name,
+            username: prismaUser.username,
+            email: prismaUser.email!,
+            emailVerified: null,
+            avatar_url: prismaUser.avatar_url!
+        }
+      },
 
       async getUser(id) {
         const user = await prisma.user.findUniqueOrThrow({
@@ -40,7 +72,7 @@ export function PrismaAdapter(): Adapter {
       },
 
       async getUserByAccount({ providerAccountId, provider }) {
-        const { user } = await prisma.account.findUniqueOrThrow({
+        const account = await prisma.account.findUnique({
             where: {
                 provider_provider_account_id: {
                     provider,
@@ -51,6 +83,12 @@ export function PrismaAdapter(): Adapter {
                 user: true
             }
         })
+
+        if(!account) {
+            return null
+        }
+
+        const { user } = account
 
         return {
             id: user.id,
@@ -119,7 +157,7 @@ export function PrismaAdapter(): Adapter {
       },
 
       async getSessionAndUser(sessionToken) {
-        const { user, ...session } = await prisma.session.findUniqueOrThrow({
+        const prismaSession = await prisma.session.findUnique({
             where: {
                 session_token: sessionToken
             },
@@ -127,6 +165,12 @@ export function PrismaAdapter(): Adapter {
                 user: true
             }
         })
+
+        if(!prismaSession) {
+            return null
+        }
+
+        const { user, ...session } = prismaSession
 
         return {
             session: {
@@ -162,5 +206,13 @@ export function PrismaAdapter(): Adapter {
             expires: prismaSession.expires
         }
       },
+
+      async deleteSession(sessionToken) {
+        await prisma.session.delete({
+            where: {
+                session_token: sessionToken
+            }
+        })
+      }
     }
   }
--- src/pages/api/auth/[...nextauth].api.ts
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -1,31 +1,37 @@
 import { PrismaAdapter } from "@/lib/auth/prisma-adapter"
-import { sign } from "crypto"
+import { NextApiRequest, NextApiResponse } from "next"
 import NextAuth, { NextAuthOptions } from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
-export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(),
 
-  providers: [
-    GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID ?? '',
-        clientSecret: process.env.GOOGLE_SECRET_KEY ?? '',
-
-        authorization: {
-          params: {
-            scope: 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/calendar'
+export function buildNextAuthOptions(req: NextApiRequest, res: NextApiResponse): NextAuthOptions {
+  return {
+    adapter: PrismaAdapter(req, res),
+  
+    providers: [
+      GoogleProvider({
+          clientId: process.env.GOOGLE_CLIENT_ID ?? '',
+          clientSecret: process.env.GOOGLE_SECRET_KEY ?? '',
+  
+          authorization: {
+            params: {
+              scope: 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/calendar'
+            }
           }
+      }),
+    ],
+  
+    callbacks: {
+      async signIn({ account }) {
+        if(!account?.scope?.includes('https://www.googleapis.com/auth/calendar')) {
+          return '/register/connect-calendar/?error=permissions'
         }
-    }),
-  ],
-
-  callbacks: {
-    async signIn({ account }) {
-      if(!account?.scope?.includes('https://www.googleapis.com/auth/calendar')) {
-        return '/register/connect-calendar/?error=permissions'
+  
+        return true
       }
-
-      return true
     }
   }
 }
-export default NextAuth(authOptions)
+
+export default async function auth(req: NextApiRequest, res: NextApiResponse) {
+  return await NextAuth(req, res, buildNextAuthOptions(req, res))
+}

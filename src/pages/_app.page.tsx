1f1dd67b7fd7eccc9f8552ef57eab58d9469fdb4
bBraian <braianvoficial@gmail.com>
2023-07-10 21:58:54 -0300

    linsting available times for each day

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,6 +1,8 @@
+import { queryClient } from '@/lib/react-query'
 import '../lib/dayjs'
 
 import { globalStyles } from '@/styles/global'
+import { QueryClientProvider } from '@tanstack/react-query'
 import { SessionProvider } from "next-auth/react"
 import type { AppProps } from 'next/app'
 
@@ -8,8 +10,10 @@ globalStyles()
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <QueryClientProvider client={queryClient}>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </QueryClientProvider>
   )
 }
